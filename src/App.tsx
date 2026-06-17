47f6eba5b6ce3d6f8fa74010a3127bd38fca2d9a
Komron Ilxomjonov <komronbek.0409@gmail.com>
2026-06-17 12:51:29 +0500

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { AppRouter } from './app/AppRouter';
 import { TelegramLayout } from './app/TelegramLayout';
 export function App() {
   return (
-    <div className="min-h-screen w-full bg-tg-bg text-tg-text antialiased selection:bg-tg-primary/30">
+    <div className="h-screen w-full bg-tg-bg text-tg-text antialiased selection:bg-tg-primary/30 overflow-y-auto overscroll-y-contain">
 
       <div
         className="flex-none bg-tg-bg"
